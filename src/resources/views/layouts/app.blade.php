4c15c390fc72ce850a4262a1ca46825aa2231adb
kotakatsumi <144547657+kotakatsumi@users.noreply.github.com>
2024-01-09 19:41:40 +0900

    再度確認

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    @yield('css')
+</head>
+<body>
+    <header>
+        <div class="header-back">
+            <h1 class="header-title font">Atte</h1>
+        </div>
+        @yield('header')
+    </header>
+    <main>
+        @yield('content')
+    </main>
+    <footer>
+        <div class="footer-back">
+            <p class="footer-content font">Atte,inc.</p>
+        </div>
+        @yield('footer')
+    </footer>
+</body>
+</html>
